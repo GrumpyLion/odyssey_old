0a52e592a8b98917dfbca315c910a633ba3fa8b0
Lion Schitik <lion@chucklefish.org>
2022-08-27 00:23:01 +0100

    Log into log.txt instead of creating a new log every startup

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -13,12 +13,8 @@ std::shared_ptr<spdlog::logger> Logger::myLogger = nullptr;
 void Logger::Initialize()
 {
 #if IS_WINDOWS_PLATFORM
-	const auto now = std::time(nullptr);
-	std::ostringstream os;
-	os << "logs/";
-	os << std::put_time(std::gmtime(&now), "%F - %H-%M-%S.txt");
-
-	myLogger = spdlog::basic_logger_mt("Engine", os.str());
+	std::remove("log.txt");
+	myLogger = spdlog::basic_logger_mt("Engine", "log.txt");
 	myLogger->sinks().push_back(std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>());
 	myLogger->sinks().push_back(std::make_shared<spdlog::sinks::msvc_sink_mt>());
 	spdlog::set_default_logger(myLogger);
